aa2d849441f126d9fcb2a0b41d6e9973f07385ba
Syver Døving Agdestein <syverda@icloud.com>
2021-09-22 10:28:13 +0200

    Format docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,27 @@
 using IncompressibleNavierStokes
 using Documenter
 
-DocMeta.setdocmeta!(IncompressibleNavierStokes, :DocTestSetup, :(using IncompressibleNavierStokes); recursive=true)
+DocMeta.setdocmeta!(
+    IncompressibleNavierStokes,
+    :DocTestSetup,
+    :(using IncompressibleNavierStokes);
+    recursive = true,
+)
 
 makedocs(;
-    modules=[IncompressibleNavierStokes],
-    authors="Syver Døving Agdestein <syverda@icloud.com> and contributors",
-    repo="https://github.com/agdestein/IncompressibleNavierStokes.jl/blob/{commit}{path}#{line}",
-    sitename="IncompressibleNavierStokes.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://agdestein.github.io/IncompressibleNavierStokes.jl",
-        assets=String[],
+    modules = [IncompressibleNavierStokes],
+    authors = "Syver Døving Agdestein <syverda@icloud.com> and contributors",
+    repo = "https://github.com/agdestein/IncompressibleNavierStokes.jl/blob/{commit}{path}#{line}",
+    sitename = "IncompressibleNavierStokes.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://agdestein.github.io/IncompressibleNavierStokes.jl",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
 deploydocs(;
-    repo="github.com/agdestein/IncompressibleNavierStokes.jl",
-    devbranch="main",
+    repo = "github.com/agdestein/IncompressibleNavierStokes.jl",
+    devbranch = "main",
 )
