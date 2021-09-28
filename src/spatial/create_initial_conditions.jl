1a23fba2c8707c2e4e08a0c7c6bff0f9716c7738
Syver Døving Agdestein <syverda@icloud.com>
2021-09-28 10:15:54 +0200

    Type steady IC

--- src/spatial/create_initial_conditions.jl
+++ src/spatial/create_initial_conditions.jl
@@ -9,12 +9,17 @@ function create_initial_conditions(setup)
     # Grid points for u, v, and p
     @unpack xu, yu, xv, yv, xpp, ypp = setup.grid
 
-    # constant velocity field
-    u = setup.case.initial_velocity_u.(xu, yu, [setup])
-    v = setup.case.initial_velocity_v.(xv, yv, [setup])
+    # Allocate
+    u = zero(xu)
+    v = zero(xv)
+    p = zero(xpp)
 
-    # pressure: should in principle NOT be prescribed. Will be calculated if p_initial
-    p = setup.case.initial_pressure.(xpp, ypp, [setup])
+    # Initial velocities
+    u .= setup.case.initial_velocity_u.(xu, yu, [setup])
+    v .= setup.case.initial_velocity_v.(xv, yv, [setup])
+
+    # Initial pressure: should in principle NOT be prescribed (will be calculated if p_initial)
+    p .= setup.case.initial_pressure.(xpp, ypp, [setup])
 
     # Stack all u components and v components
     V = [u[:]; v[:]];
