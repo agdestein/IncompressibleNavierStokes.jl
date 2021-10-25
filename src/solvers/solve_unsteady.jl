1e12aa9c67f212ebb60632e71c720a190ada6cba
agdestein <syverda@icloud.com>
2021-10-25 11:07:30 +0200

    Fix siganature in function call

--- src/solvers/solve_unsteady.jl
+++ src/solvers/solve_unsteady.jl
@@ -12,12 +12,14 @@ function solve(::UnsteadyProblem, setup, V₀, p₀)
 
     # For methods that need a velocity field at n-1 the first time step
     # (e.g. AB-CN, oneleg beta) use ERK or IRK
-    stepper = if needs_startup_method(method)
+    if needs_startup_method(method)
         println("Starting up with method $(method_startup)")
-        TimeStepper(method_startup, setup, V₀, p₀)
+        method_use = method_startup
     else
-        TimeStepper(method, setup, V₀, p₀)
+        method_use = method
     end
+
+    stepper = TimeStepper(method_use, setup, V₀, p₀)
 
     # Initialize BC arrays
     set_bc_vectors!(setup, stepper.t)
@@ -29,12 +31,13 @@ function solve(::UnsteadyProblem, setup, V₀, p₀)
     while stepper.t < t_end
         if stepper.n == nstartup && needs_startup_method(method)
             println("n = $(stepper.n): switching to primary ODE method ($method)")
+            method_use = method
             stepper = change_time_stepper(stepper, method)
         end
 
         # Change timestep based on operators
         if isadaptive && rem(stepper.n, n_adapt_Δt) == 0
-            Δt = get_timestep(stepper, setup)
+            Δt = get_timestep(setup, method_use)
         end
 
         # Perform a single time step with the time integration method
