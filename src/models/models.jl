b3d6cef2aa3a55f65dd4444b3a98686db85048bd
agdestein <syverda@icloud.com>
2021-10-29 11:01:28 +0200

    Rename

--- src/models/models.jl
+++ src/models/models.jl
@@ -1,11 +1,11 @@
-abstract type ViscosityModel{T} end
+abstract type AbstractViscosityModel{T} end
 
-struct LaminarModel{T} <: ViscosityModel{T} end
-struct KEpsilonModel{T} <: ViscosityModel{T} end
-Base.@kwdef struct MixingLengthModel{T} <: ViscosityModel{T}
+struct LaminarModel{T} <: AbstractViscosityModel{T} end
+struct KEpsilonModel{T} <: AbstractViscosityModel{T} end
+Base.@kwdef struct MixingLengthModel{T} <: AbstractViscosityModel{T}
     lm::T = 1 # Mixing length
 end
-Base.@kwdef struct SmagorinskyModel{T} <: ViscosityModel{T}
+Base.@kwdef struct SmagorinskyModel{T} <: AbstractViscosityModel{T}
     C_s::T = 0.17 # Smagorinsky constant
 end
-struct QRModel{T} <: ViscosityModel{T} end
+struct QRModel{T} <: AbstractViscosityModel{T} end
