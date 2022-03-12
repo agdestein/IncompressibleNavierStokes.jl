c8ea4457d76dc055d0735f0a87c981a38d64b236
Documenter.jl <documenter@juliadocs.github.io>
2022-03-12 11:46:34 +0000

    build based on 8af372b

--- dev/generated/LidDrivenCavity2D.jl
+++ dev/generated/LidDrivenCavity2D.jl
@@ -59,8 +59,8 @@ V, p = @time solve(problem)
 
 problem = UnsteadyProblem(setup, V₀, p₀, tlims)
 
-logger = Logger(; nupdate = 20)
-plotter = RealTimePlotter(; nupdate = 20, fieldname = :vorticity)
+logger = Logger(; nupdate = 1)
+plotter = RealTimePlotter(; nupdate = 50, fieldname = :vorticity, type = contourf)
 writer = VTKWriter(; nupdate = 20, dir = "output/LidDrivenCavity2D")
 tracer = QuantityTracer(; nupdate = 10)
 processors = [logger, plotter, writer, tracer]
