640a32b33b7737893f2ed08abbac4584a1e8b52f
Syver Døving Agdestein <syverda@gmail.com>
2023-06-30 15:56:17 +0200

    Use named tuples for cache

--- src/momentum/momentumcache.jl
+++ src/momentum/momentumcache.jl
@@ -1,88 +1,98 @@
 """
-    MomentumCache
+    MomentumCache(setup)
 
 Preallocation structure for terms in the momentum equations.
 """
-Base.@kwdef struct MomentumCache{T}
-    Gp::Vector{T}
-    c::Vector{T}
-    c2::Vector{T}
-    c3::Vector{T}
-    d::Vector{T}
-    b::Vector{T}
-    ∇c::SparseMatrixCSC{T,Int}
-    ∇c2::SparseMatrixCSC{T,Int}
-    ∇c3::SparseMatrixCSC{T,Int}
-    ∇d::SparseMatrixCSC{T,Int}
-    ∇b::SparseMatrixCSC{T,Int}
-    u_ux::Vector{T}
-    ū_ux::Vector{T}
-    uū_ux::Vector{T}
+MomentumCache(setup) = MomentumCache(setup.grid.dimension, setup)
 
-    u_uy::Vector{T}
-    v̄_uy::Vector{T}
-    uv̄_uy::Vector{T}
+function MomentumCache(::Dimension{2}, setup)
+    (; grid, operators) = setup
+    (; Nu, Nv, NV) = grid
+    (; Iu_ux, Iv_uy, Iu_vx, Iv_vy) = operators
 
-    u_uz::Vector{T}
-    w̄_uz::Vector{T}
-    uw̄_uz::Vector{T}
+    T = eltype(Iu_ux)
 
-    v_vx::Vector{T}
-    ū_vx::Vector{T}
-    vū_vx::Vector{T}
+    Gp = zeros(T, NV)
+    c = zeros(T, NV)
+    c2 = zeros(T, NV)
+    c3 = zeros(T, NV)
+    d = zeros(T, NV)
+    b = zeros(T, NV)
+    ∇c = spzeros(T, NV, NV)
+    ∇c2 = spzeros(T, NV, NV)
+    ∇c3 = spzeros(T, NV, NV)
+    ∇d = spzeros(T, NV, NV)
+    ∇b = spzeros(T, NV, NV)
 
-    v_vy::Vector{T}
-    v̄_vy::Vector{T}
-    vv̄_vy::Vector{T}
+    u_ux = zeros(T, size(Iu_ux, 1))
+    ū_ux = zeros(T, size(Iu_ux, 1))
+    uū_ux = zeros(T, size(Iu_ux, 1))
 
-    v_vz::Vector{T}
-    w̄_vz::Vector{T}
-    vw̄_vz::Vector{T}
+    u_uy = zeros(T, size(Iv_uy, 1))
+    v̄_uy = zeros(T, size(Iv_uy, 1))
+    uv̄_uy = zeros(T, size(Iv_uy, 1))
 
-    w_wx::Vector{T}
-    ū_wx::Vector{T}
-    wū_wx::Vector{T}
+    v_vx = zeros(T, size(Iu_vx, 1))
+    ū_vx = zeros(T, size(Iu_vx, 1))
+    vū_vx = zeros(T, size(Iu_vx, 1))
 
-    w_wy::Vector{T}
-    v̄_wy::Vector{T}
-    wv̄_wy::Vector{T}
+    v_vy = zeros(T, size(Iv_vy, 1))
+    v̄_vy = zeros(T, size(Iv_vy, 1))
+    vv̄_vy = zeros(T, size(Iv_vy, 1))
 
-    w_wz::Vector{T}
-    w̄_wz::Vector{T}
-    ww̄_wz::Vector{T}
+    ∂uū∂x = zeros(T, Nu)
+    ∂uv̄∂y = zeros(T, Nu)
+    ∂vū∂x = zeros(T, Nv)
+    ∂vv̄∂y = zeros(T, Nv)
 
-    ∂uū∂x::Vector{T}
-    ∂uv̄∂y::Vector{T}
-    ∂uw̄∂z::Vector{T}
-    ∂vū∂x::Vector{T}
-    ∂vv̄∂y::Vector{T}
-    ∂vw̄∂z::Vector{T}
-    ∂wū∂x::Vector{T}
-    ∂wv̄∂y::Vector{T}
-    ∂ww̄∂z::Vector{T}
+    Conv_ux_11 = spzeros(T, Nu, Nu)
+    Conv_uy_11 = spzeros(T, Nu, Nu)
+    Conv_uy_12 = spzeros(T, Nu, Nv)
 
-    Conv_ux_11::SparseMatrixCSC{T,Int}
-    Conv_uy_11::SparseMatrixCSC{T,Int}
-    Conv_uz_11::SparseMatrixCSC{T,Int}
-    Conv_uy_12::SparseMatrixCSC{T,Int}
-    Conv_uz_13::SparseMatrixCSC{T,Int}
+    Conv_vx_21 = spzeros(T, Nv, Nu)
+    Conv_vx_22 = spzeros(T, Nv, Nv)
+    Conv_vy_22 = spzeros(T, Nv, Nv)
 
-    Conv_vx_21::SparseMatrixCSC{T,Int}
-    Conv_vx_22::SparseMatrixCSC{T,Int}
-    Conv_vy_22::SparseMatrixCSC{T,Int}
-    Conv_vz_22::SparseMatrixCSC{T,Int}
-    Conv_vz_23::SparseMatrixCSC{T,Int}
-
-    Conv_wx_31::SparseMatrixCSC{T,Int}
-    Conv_wy_32::SparseMatrixCSC{T,Int}
-    Conv_wx_33::SparseMatrixCSC{T,Int}
-    Conv_wy_33::SparseMatrixCSC{T,Int}
-    Conv_wz_33::SparseMatrixCSC{T,Int}
+    (;
+        Gp,
+        c,
+        c2,
+        c3,
+        d,
+        b,
+        ∇c,
+        ∇c2,
+        ∇c3,
+        ∇d,
+        ∇b,
+        u_ux,
+        ū_ux,
+        uū_ux,
+        u_uy,
+        v̄_uy,
+        uv̄_uy,
+        v_vx,
+        ū_vx,
+        vū_vx,
+        v_vy,
+        v̄_vy,
+        vv̄_vy,
+        ∂uū∂x,
+        ∂uv̄∂y,
+        ∂vū∂x,
+        ∂vv̄∂y,
+        Conv_ux_11,
+        Conv_uy_11,
+        Conv_uy_12,
+        Conv_vx_21,
+        Conv_vx_22,
+        Conv_vy_22,
+    )
 end
 
-function MomentumCache(setup)
+function MomentumCache(::Dimension{3}, setup)
     (; grid, operators) = setup
-    (; Nu, Nv, Nw, NV, order4) = grid
+    (; Nu, Nv, Nw, NV) = grid
     (; Iu_ux, Iv_uy, Iw_uz, Iu_vx, Iv_vy, Iw_vz, Iu_wx, Iv_wy, Iw_wz) = operators
 
     T = eltype(Iu_ux)
@@ -163,7 +173,7 @@ function MomentumCache(setup)
     Conv_wy_33 = spzeros(T, Nw, Nw)
     Conv_wz_33 = spzeros(T, Nw, Nw)
 
-    MomentumCache{T}(;
+    (;
         Gp,
         c,
         c2,
