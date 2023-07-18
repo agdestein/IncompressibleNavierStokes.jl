2a3e5e0e1dcf878f6d34a3144039dd29a33a77b7
Henrik Rosenberger <hker@cwi.nl>
2023-07-18 17:51:48 +0200

    start implementation snapshot production

--- examples/DecayingTurbulence2D_snapshot_production.jl
+++ examples/DecayingTurbulence2D_snapshot_production.jl
@@ -26,7 +26,7 @@ using GLMakie #!md
 using IncompressibleNavierStokes
 
 # Case name for saving results
-name = "DecayingTurbulence2D"
+name = "DecayingTurbulence2D_snapshot_production"
 
 # Floating point precision
 T = Float32
@@ -43,7 +43,7 @@ device = identity
 viscosity_model = LaminarModel(; Re = T(10_000))
 
 # A 2D grid is a Cartesian product of two vectors
-n = 256
+n = 256 # 1024
 lims = (T(0), T(1))
 x = LinRange(lims..., n + 1)
 y = LinRange(lims..., n + 1)
@@ -56,35 +56,50 @@ setup = Setup(x, y; viscosity_model);
 # spectral pressure solver
 pressure_solver = SpectralPressureSolver(setup);
 
-# Initial conditions
-V₀, p₀ = random_field(setup; A = T(1_000_000), σ = T(30), s = 5, pressure_solver);
+K = 2; 
 
-# Iteration processors
-processors = (
-    field_plotter(device(setup); nupdate = 20),
-    energy_history_plotter(device(setup); nupdate = 20, displayfig = false),
-    energy_spectrum_plotter(device(setup); nupdate = 20, displayfig = false),
-    ## animator(device(setup), "vorticity.mp4"; nupdate = 16),
-    ## vtk_writer(setup; nupdate = 10, dir = "output/$name", filename = "solution"),
-    ## field_saver(setup; nupdate = 10),
-    step_logger(; nupdate = 100),
-);
+snapshots_V₀ = zeros(setup.grid.NV,K);
+snapshots_V  = zeros(setup.grid.NV,K);
 
-# Time interval
-t_start, t_end = tlims = (T(0), T(1.0))
+for k = 1:K
+    # Initial conditions
+    V₀, p₀ = random_field(setup; A = T(1_000_000), σ = T(30), s = 5, pressure_solver);
 
-# Solve unsteady problem
-V, p, outputs = solve_unsteady(
-    setup,
-    V₀,
-    p₀,
-    tlims;
-    Δt = T(0.001),
-    processors,
-    pressure_solver,
-    inplace = true,
-    device,
-);
+    snapshots_V₀[:,k] = V₀;
+
+    # Iteration processors
+    processors = (
+        field_plotter(device(setup); nupdate = 1,displayfig = false),
+        energy_history_plotter(device(setup); nupdate = 20, displayfig = false),
+        energy_spectrum_plotter(device(setup); nupdate = 1, displayfig = true),
+        ## animator(device(setup), "vorticity.mp4"; nupdate = 16),
+        ## vtk_writer(setup; nupdate = 10, dir = "output/$name", filename = "solution"),
+        ## field_saver(setup; nupdate = 10),
+        step_logger(; nupdate = 100),
+    );
+
+    # Time interval
+    t_start, t_end = tlims = (T(0), T(.01))
+
+    # Solve unsteady problem
+    V, p, outputs = solve_unsteady(
+        setup,
+        V₀,
+        p₀,
+        tlims;
+        Δt = T(0.001),
+        processors,
+        pressure_solver,
+        inplace = true,
+        device,
+    );
+
+    snapshots_V[:,k] = V;
+end
+
+
+
+
 
 # Field plot
 outputs[1]
@@ -100,7 +115,7 @@ outputs[3]
 # We may visualize or export the computed fields `(V, p)`
 
 # Export to VTK
-save_vtk(setup, V, p, t_end, "output/solution")
+# save_vtk(setup, V, p, t_end, "output/solution")
 
 # Plot pressure
 plot_pressure(setup, p)
