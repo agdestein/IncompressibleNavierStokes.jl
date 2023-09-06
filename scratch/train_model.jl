247c12c7ad725c669e129123192f6ba10f1f674f
Syver Døving Agdestein <syverda@gmail.com>
2023-09-06 10:51:26 +0200

    Add force

--- scratch/train_model.jl
+++ scratch/train_model.jl
@@ -35,96 +35,130 @@ device = cu
 # Setup
 n = 128
 lims = T(0), T(1)
-viscosity_model = LaminarModel(; Re = T(2_000))
-t_burn = T(0.1)
-t_sim = T(0.1)
+viscosity_model = LaminarModel(; Re = T(6_000))
+tburn = T(0.05)
+tsim = T(0.05)
 
 # Build LES setup and assemble operators
 x = LinRange(lims..., n + 1)
 y = LinRange(lims..., n + 1)
-setup = Setup(x, y; viscosity_model)
+setup = Setup(x, y; viscosity_model);
+
+# Number of simulations
+ntrain = 10
+nvalid = 2
+ntest = 5
 
 # Create LES data from DNS
-filtered = create_les_data(
-    T;
+params = (;
     viscosity_model,
     lims,
-    n_les = n,
+    nles = n,
     compression = 4,
-    n_ic = 10,
-    t_burn,
-    t_sim,
-    Δt = T(2e-4),
+    tburn,
+    tsim,
+    Δt = T(1e-4),
     device,
 )
-jldsave("output/filtered/filtered.jld2"; filtered)
+data_train = create_les_data(T; params..., nsim = ntrain)
+data_valid = create_les_data(T; params..., nsim = nvalid)
+data_test = create_les_data(T; params..., nsim = ntest)
+
+# jldsave("output/filtered/data.jld2"; data_train, data_valid, data_test)
 
 # Load previous LES data
-filtered = load("output/filtered/filtered.jld2", "filtered")
+data_train, data_valid, data_test = load("output/filtered/data.jld2", "data_train", "data_valid", "data_test")
 
-size(filtered.V)
+nt = size(data_train.V, 2) - 1
+
+size(data_train.V)
+size(data_valid.V)
+size(data_test.V)
 
 # Inspect data
-plot_vorticity(setup, filtered.V[:, end, 1], T(0))
+plot_vorticity(setup, data_valid.V[:, 1, 1], T(0))
+plot_vorticity(setup, data_valid.V[:, end, 1], T(0))
+norm(data_valid.cF[:, 1, 1]) / norm(data_valid.F[:, 1, 1])
+norm(data_valid.cF[:, end, 1]) / norm(data_valid.F[:, end, 1])
 
 # Uniform periodic grid
-pressure_solver = SpectralPressureSolver(setup)
+pressure_solver = SpectralPressureSolver(setup);
 
-# Compute commutator errors
-_, n_t, n_ic = size(filtered.V)
-bc_vectors = get_bc_vectors(setup, T(0))
-commutator_error = zero(filtered.F)
-pbar = filtered.p[:, 1, 1]
-for i_t = 1:n_t, i_ic = 1:n_ic
-    @info "Computing commutator error for time $i_t of $n_t, IC $i_ic of $n_ic"
-    V = filtered.V[:, i_t, i_ic]
-    F = filtered.F[:, i_t, i_ic]
-    Fbar, = momentum(V, V, pbar, T(0), setup; bc_vectors, nopressure = true)
-    commutator_error[:, i_t, i_ic] .= F .- Fbar
+q = data_valid.V
+# q = data_train.FG
+q = q[:, :, 1]
+q = selectdim(reshape(q, n, n, 2, :), 3, 1)
+
+qc = reshape(selectdim(data_valid.cF, 3, 2), n, n, :)
+
+obs = Observable(randn(T, 1, 1))
+# obs = Observable(selectdim(q, 3, 1))
+# obs = Observable([selectdim(q, 3, 1) selectdim(qc, 3, 1)])
+fig = heatmap(obs)
+fig
+
+# for snap in eachslice(q; dims = 3)
+for (s1, s2) in zip(eachslice(q; dims = 3), eachslice(qc; dims = 3))
+    obs[] = s1
+    # obs[] = [s1 s2]
+    autolimits!(fig.axis)
+    sleep(0.005)
 end
 
-norm(commutator_error[:, 1, 1]) / norm(filtered.F[:, 1, 1])
+heatmap(selectdim(reshape(data_valid.force[:, 1], n, n, 2), 3, 1))
+
+fx, fy = eachslice(reshape(data_valid.force[:, 1], n, n, 2); dims = 3)
+heatmap(fx)
+arrows(x, y, fx, fy; lengthscale = 1.0f0)
 
 # closure, θ₀ = cnn(
 #     setup,
+#
+#     # Radius
 #     [5, 5, 5],
-#     [2, 8, 8, 2],
+#
+#     # Channels
+#     [2, 64, 64, 2],
+#
+#     # Activations
 #     [leakyrelu, leakyrelu, identity],
+#
+#     # Bias
 #     [true, true, false];
 # )
 
 closure, θ₀ = fno(
-    # Setup
     setup,
 
     # Cut-off wavenumbers
-    [8, 8, 8],
+    [32, 32, 32, 32],
 
     # Channel sizes
-    [16, 8, 8],
+    [24, 12, 8, 8],
 
-    # Fourier activations
-    [gelu, gelu, identity],
+    # Fourier layer activations
+    [gelu, gelu, gelu, identity],
 
     # Dense activation
     gelu,
-)
+);
 
 @info "Closure model has $(length(θ₀)) parameters"
 
 # Test data
-V_test = device(reshape(filtered.V[:, 1:20, 1:2], :, 40))
-c_test = device(reshape(commutator_error[:, 1:20, 1:2], :, 40))
+V_test = device(reshape(data_test.V[:, 1:20, 1:2], :, 40))
+c_test = device(reshape(data_test.cF[:, 1:20, 1:2], :, 40))
 
 # Prepare training
-θ = 5.0f-2 * device(θ₀)
-opt = Optimisers.setup(Adam(1.0f-2), θ)
+θ = 1.0f-1 * device(θ₀)
+# θ = device(θ₀)
+opt = Optimisers.setup(Adam(1.0f-3), θ)
 callbackstate = Point2f[]
 randloss = create_randloss(
     mean_squared_error,
     closure,
-    filtered.V,
-    commutator_error;
+    data_train.V,
+    data_train.cF;
     nuse = 50,
     device,
 )
@@ -143,63 +177,141 @@ first(gradient(randloss, θ));
     randloss,
     opt,
     θ;
-    niter = 500,
+    niter = 1000,
     ncallback = 10,
     callbackstate,
     callback = create_callback(closure, V_test, c_test; state = callbackstate),
 )
+GC.gc()
+CUDA.reclaim()
 
+Array(θ)
+
+# # Save trained parameters
 # jldsave("output/theta.jld2"; θ = Array(θ))
+
+# # Load trained parameters
 # θθ = load("output/theta.jld2")
 # θθ = θθ["θ"]
 # θθ = cu(θθ)
 # θ .= θθ
 
+relative_error(closure(device(data_train.V[:, 1, :]), θ), device(data_train.cF[:, 1, :]))
+relative_error(closure(device(data_train.V[:, end, :]), θ), device(data_train.cF[:, end, :]))
 relative_error(closure(V_test, θ), c_test)
 
-devsetup = device(setup);
+function energy_history(setup, state)
+    (; Ωp) = setup.grid
+    points = Point2f[]
+    on(state) do (; V, p, t)
+        V = Array(V)
+        vels = get_velocity(setup, V, t)
+        vels = reshape.(vels, :)
+        E = sum(vel -> sum(@. Ωp * vel^2), vels)
+        push!(points, Point2f(t, E))
+    end
+    points
+end
 
+energy_history_writer(setup; nupdate = 1, kwargs...) =
+    processor(state -> energy_history(setup, state; kwargs...); nupdate)
+
+devsetup = device(setup);
 V_nm, p_nm, outputs_nm = solve_unsteady(
     setup,
-    filtered.V[:, 1, 1],
-    filtered.p[:, 1, 1],
-    (T(0), t_sim);
+    data_train.V[:, 1, 1],
+    data_train.p[:, 1, 1],
+    (T(0), tsim);
     Δt = T(2e-4),
     processors = (
-        step_logger(; nupdate = 10),
         field_plotter(devsetup; type = heatmap, nupdate = 1),
+        energy_history_writer(setup),
+        step_logger(; nupdate = 10),
     ),
     pressure_solver,
     inplace = false,
     device,
     devsetup,
 )
+ehist_nm = outputs_nm[2]
 
+setup_fno = (; setup..., closure_model = V -> closure(V, θ))
+devsetup = device(setup_fno);
 V_fno, p_fno, outputs_fno = solve_unsteady(
-    (; setup..., closure_model = V -> closure(V, θ)),
-    filtered.V[:, 1, 1],
-    filtered.p[:, 1, 1],
-    (T(0), t_sim);
+    setup_fno,
+    data_train.V[:, 1, 1],
+    data_train.p[:, 1, 1],
+    (T(0), tsim);
     Δt = T(2e-4),
     processors = (
-        step_logger(; nupdate = 10),
         field_plotter(devsetup; type = heatmap, nupdate = 1),
+        energy_history_writer(setup),
+        step_logger(; nupdate = 10),
     ),
     pressure_solver,
     inplace = false,
     device,
     devsetup,
 )
+ehist_fno = outputs_fno[2]
 
-V = filtered.V[:, end, 1]
-p = filtered.p[:, end, 1]
+state = Observable((; V = data_train.V[:, 1, 1], p = data_train.p[:, 1, 1], t = T(0)))
+ehist = energy_history(setup, state)
+for i = 2:nt+1
+    t = (i - 1) / T(nt - 1) * tsim
+    V = data_train.V[:, i, 1]
+    p = data_train.p[:, i, 1]
+    state[] = (; V, p, t)
+end
+ehist
+
+fig = Figure()
+ax = Axis(fig[1, 1]; xlabel = "t", ylabel = "Kinetic energy")
+lines!(ax, ehist; label = "Reference")
+lines!(ax, ehist_nm; label = "No closure")
+lines!(ax, ehist_fno; label = "FNO")
+axislegend(ax)
+fig
+
+save("output/train/energy.png", fig)
+
+V = data_train.V[:, end, 1]
+p = data_train.p[:, end, 1]
 
 relative_error(V_nm, V)
 relative_error(V_fno, V)
 
-plot_vorticity(setup, V_nm, t_sim)
-plot_vorticity(setup, V_fno, t_sim)
-plot_vorticity(setup, V, t_sim)
+box = [
+    Point2f(0.72, 0.42),
+    Point2f(0.81, 0.42),
+    Point2f(0.81, 0.51),
+    Point2f(0.72, 0.51),
+    Point2f(0.72, 0.42),
+]
+
+plot_vorticity(setup, V, tsim)
+lines!(box; color = :red)
+current_figure()
+
+save("output/train/vorticity.png", current_figure())
+
+plot_vorticity(setup, V_nm, tsim)
+lines!(box; color = :red)
+current_figure()
+
+save("output/train/vorticity_nm.png", current_figure())
+
+plot_vorticity(setup, V_fno, tsim)
+lines!(box; color = :red)
+current_figure()
+
+save("output/train/vorticity_fno.png", current_figure())
+
+heatmap(vcat(
+    selectdim(reshape(V_nm, n, n, 2), 3, 1),
+    # selectdim(reshape(V_fno, n, n, 2), 3, 1),
+    selectdim(reshape(V, n, n, 2), 3, 1),
+))
 
 CUDA.memory_status()
 GC.gc()
--- src/closures/create_les_data.jl
+++ src/closures/create_les_data.jl
@@ -1,3 +1,37 @@
+# Body force
+function gaussian_force(
+    x,
+    y;
+    σ = eltype(x)(0.05),
+    A = eltype(x)(0.002),
+    rng = Random.default_rng(),
+)
+    T = eltype(x)
+    Lx = x[end] - x[1]
+    Ly = y[end] - y[1]
+    xc = x[1] + rand(rng, T) * Lx
+    yc = y[1] + rand(rng, T) * Ly
+    σx = σ * Lx
+    σy = σ * Ly
+    ϕ = T(2π) * rand(rng, T)
+    f = sum(
+        [
+            A * exp(-(x - xc - lx)^2 / 2σx^2 - (y - yc - ly)^2 / 2σy^2) for
+            y ∈ y[2:end], x ∈ x[2:end], ly ∈ (-Ly, T(0), Ly), lx ∈ (-Lx, T(0), Lx)
+        ];
+        dims = (3, 4),
+    )[
+        :,
+        :,
+        1,
+        1,
+    ]
+    force = cat(sin(ϕ) * f, cos(ϕ) * f; dims = 3)
+    force = reshape(force, :)
+    force = force .- sum(force) / length(force)
+    force
+end
+
 _filter_saver(
     dns,
     les,
@@ -92,6 +126,7 @@ function create_les_data(
 
     # Filter
     (; KV, Kp) = operator_filter(dns.grid, dns.boundary_conditions, compression)
+    KVmom = Diagonal(les.grid.Ω) * (KV * Diagonal(1 ./ dns.grid.Ω))
 
     # Since the grid is uniform and identical for x and y, we may use a specialized
     # spectral pressure solver
@@ -109,22 +144,10 @@ function create_les_data(
         FG = zeros(T, nles * nles * 2, nt + 1, nsim),
         cF = zeros(T, nles * nles * 2, nt + 1, nsim),
         cFG = zeros(T, nles * nles * 2, nt + 1, nsim),
+        force = zeros(T, nles * nles * 2, nsim),
     )
 
     @info "Generating $(Base.summarysize(filtered) / 1e6) Mb of LES data"
-
-    # Iteration processors
-    processors = (
-        _filter_saver(
-            device(dns),
-            device(les),
-            device(KV),
-            device(Kp);
-            bc_vectors_dns = device(get_bc_vectors(dns, T(0))),
-            bc_vectors_les = device(get_bc_vectors(les, T(0))),
-        ),
-        step_logger(; nupdate = 10),
-    )
 
     for isim = 1:nsim
         @info "Generating data for simulation $isim of $nsim"
@@ -132,10 +155,21 @@ function create_les_data(
         # Initial conditions
         V₀, p₀ = random_field(dns; A = T(10_000_000), σ = T(30), s = 5, pressure_solver)
 
+        # Random body force
+        force_dns = gaussian_force(xdns, ydns) +
+            gaussian_force(xdns, ydns) +
+            # gaussian_force(xdns, ydns) +
+            # gaussian_force(xdns, ydns) +
+            gaussian_force(xdns, ydns)
+        force_les = KVmom * force_dns
+
+        _dns = (; dns..., force = force_dns)
+        _les = (; les..., force = force_les)
+
         # Solve burn-in DNS
         @info "Burn-in for simulation $isim of $nsim"
         V, p, outputs = solve_unsteady(
-            dns,
+            _dns,
             V₀,
             p₀,
             (T(0), tburn);
@@ -149,12 +183,22 @@ function create_les_data(
         # Solve DNS and store filtered quantities
         @info "Solving DNS for simulation $isim of $nsim"
         V, p, outputs = solve_unsteady(
-            dns,
+            _dns,
             V,
             p,
             (T(0), tsim);
             Δt,
-            processors,
+            processors = (
+                _filter_saver(
+                    device(_dns),
+                    device(_les),
+                    device(KV),
+                    device(Kp);
+                    bc_vectors_dns = device(get_bc_vectors(_dns, T(0))),
+                    bc_vectors_les = device(get_bc_vectors(_les, T(0))),
+                ),
+                step_logger(; nupdate = 10),
+            ),
             pressure_solver,
             inplace = true,
             device,
@@ -168,6 +212,7 @@ function create_les_data(
         filtered.FG[:, :, isim] = stack(f.FG)
         filtered.cF[:, :, isim] = stack(f.cF)
         filtered.cFG[:, :, isim] = stack(f.cFG)
+        filtered.force[:, isim] = force_les
     end
 
     filtered
