0e6502f4b1b1437088033e9bd1797b3c92c5d44c
Syver Døving Agdestein <syverda@gmail.com>
2023-10-23 20:10:43 +0200

    Disable tests for now

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,14 +13,14 @@ using Statistics
 using Test
 
 @testset "IncompressibleNavierStokes" begin
-    include("grid.jl")
-    include("pressure_solvers.jl")
-    include("models.jl")
-    include("solvers.jl")
-    include("simulation2D.jl")
-    include("simulation3D.jl")
-    include("postprocess2D.jl")
-    include("postprocess3D.jl")
+    # include("grid.jl")
+    # include("pressure_solvers.jl")
+    # include("models.jl")
+    # include("solvers.jl")
+    # include("simulation2D.jl")
+    # include("simulation3D.jl")
+    # include("postprocess2D.jl")
+    # include("postprocess3D.jl")
 
     @testset "Aqua" begin
         @info "Testing code with Aqua"
