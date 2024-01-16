d37206f5514e651a83cf38165f6a5a6044b77f04
Syver Døving Agdestein <syverda@gmail.com>
2024-01-16 16:58:09 +0100

    fix: Volume average

--- src/filter.jl
+++ src/filter.jl
@@ -40,24 +40,37 @@ face_average(u, setup_les, comp) = face_average!(
 Average fine grid `u` over coarse volume. Put result in `v`.
 """
 function volume_average!(v, u, setup_les, comp)
-    (; grid, workgroupsize) = setup_les
-    (; Nu, Iu) = grid
+    (; grid, boundary_conditions, workgroupsize) = setup_les
+    (; N, Nu, Iu) = grid
     D = length(u)
     δ = Offset{D}()
+    @assert all(bc -> bc[1] isa PeriodicBC && bc[2] isa PeriodicBC, boundary_conditions)
     @kernel function Φ!(v, u, ::Val{α}, volume, I0) where {α}
         I = @index(Global, Cartesian)
         J = I0 + comp * (I - oneunit(I))
         s = zero(eltype(v[α]))
         for i in volume
-            s += u[α][J+i]
+            # Periodic extension
+            K = J+i
+            K = mod1.(K.I, comp .* (N .- 2))
+            K = CartesianIndex(K)
+            s += u[α][K]
         end
-        v[α][I0+I] = s / comp^D
+        n = (iseven(comp) ? comp : comp + 1) * comp^(D - 1)
+        v[α][I0+I] = s / n
     end
     for α = 1:D
         ndrange = Nu[α]
         I0 = first(Iu[α])
         I0 -= oneunit(I0)
-        volume = CartesianIndices(ntuple(β -> 1:comp, D))
+        volume = CartesianIndices(
+            ntuple(
+                β ->
+                    α == β ? iseven(comp) ? (comp÷2:comp÷2+comp)
+                             : (comp+1:2comp+1) : (1:comp),
+                D,
+            ),
+        )
         Φ!(get_backend(v[1]), workgroupsize)(v, u, Val(α), volume, I0; ndrange)
     end
     v
