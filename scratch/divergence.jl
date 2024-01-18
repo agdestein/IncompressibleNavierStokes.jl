da341d4feddc350dd6ff1c066f70a2b676a589b1
Syver Døving Agdestein <syverda@gmail.com>
2024-01-18 17:04:33 +0100

    Update script

--- scratch/divergence.jl
+++ scratch/divergence.jl
@@ -6,9 +6,11 @@ if isdefined(@__MODULE__, :LanguageServer)          #src
 end                                                 #src
 
 #md using CairoMakie
-using GLMakie #!md
+using GLMakie
 using IncompressibleNavierStokes
+using IncompressibleNavierStokes: momentum!, divergence!, project!, apply_bc_u!
 using LinearAlgebra
+using Printf
 
 # Output directory
 output = "output/DecayingTurbulence2D"
@@ -23,35 +25,129 @@ ArrayType = Array
 ## using oneAPI; ArrayType = oneArray
 ## using Metal; ArrayType = MtlArray
 
+function observe_v(dnsobs, Φ, les, compression, psolver)
+    (; ArrayType, grid) = les
+    (; dimension, N, Iu, Ip) = grid
+    D = dimension()
+    Mα = N[1] - 2
+    v = zero.(Φ(dnsobs[].u, les, compression))
+    Pv = zero.(v)
+    p = zero(v[1])
+    div = zero(p)
+    ΦPF = zero.(v)
+    PFΦ = zero.(v)
+    c = zero.(v)
+    T = eltype(v[1])
+    results = (;
+        Φ,
+        Mα,
+        t = zeros(T, 0),
+        Dv = zeros(T, 0),
+        Pv = zeros(T, 0),
+        Pc = zeros(T, 0),
+        c = zeros(T, 0),
+    )
+    on(dnsobs) do (; u, PF, t)
+        push!(results.t, t)
+
+        Φ(v, u, les, compression)
+        apply_bc_u!(v, t, les)
+        Φ(ΦPF, PF, les, compression)
+        momentum!(PFΦ, v, t, les)
+        apply_bc_u!(PFΦ, t, les; dudt = true)
+        project!(PFΦ, les; psolver, div, p)
+        foreach(α -> c[α] .= ΦPF[α] .- PFΦ[α], 1:D)
+        apply_bc_u!(c, t, les)
+        divergence!(div, v, les)
+        norm_Du = norm(div[Ip])
+        norm_v = sqrt(sum(α -> sum(abs2, v[α][Iu[α]]), 1:D))
+        push!(results.Dv, norm_Du / norm_v)
+
+        copyto!.(Pv, v)
+        project!(Pv, les; psolver, div, p)
+        foreach(α -> Pv[α] .= Pv[α] .- v[α], 1:D)
+        norm_vmPv = sqrt(sum(α -> sum(abs2, Pv[α][Iu[α]]), 1:D))
+        push!(results.Pv, norm_vmPv / norm_v)
+
+        Pc = Pv
+        copyto!.(Pc, c)
+        project!(Pc, les; psolver, div, p)
+        foreach(α -> Pc[α] .= Pc[α] .- c[α], 1:D)
+        norm_cmPc = sqrt(sum(α -> sum(abs2, Pc[α][Iu[α]]), 1:D))
+        norm_c = sqrt(sum(α -> sum(abs2, c[α][Iu[α]]), 1:D))
+        push!(results.Pc, norm_cmPc / norm_c)
+
+        norm_ΦPF = sqrt(sum(α -> sum(abs2, ΦPF[α][Iu[α]]), 1:D))
+        push!(results.c, norm_c / norm_ΦPF)
+    end
+    results
+end
+
+observe_u(dns, psolver_dns, filters; nupdate = 1) =
+    processor() do state
+        (; dimension, x) = dns.grid
+        T = eltype(x[1])
+        D = dimension()
+        PF = zero.(state[].u)
+        div = zero(state[].u[1])
+        p = zero(state[].u[1])
+        dnsobs = Observable((; state[].u, PF, state[].t))
+        results = [
+            observe_v(dnsobs, Φ, setup, compression, psolver) for
+            (; setup, Φ, compression, psolver) in filters
+        ]
+        on(state) do (; u, t, n)
+            n % nupdate == 0 || return
+            apply_bc_u!(u, t, dns)
+            momentum!(PF, u, t, dns)
+            apply_bc_u!(PF, t, dns; dudt = true)
+            project!(PF, dns; psolver = psolver_dns, div, p)
+            dnsobs[] = (; u, PF, t)
+        end
+        # state[] = state[] # Save initial conditions
+        results
+    end
+
 # Viscosity model
 Re = T(10_000)
 
 # A 2D grid is a Cartesian product of two vectors
-nles = 256
-comp = 2
-ndns = nles * comp
+ndns = 1024
 lims = T(0), T(1)
 D = 2
 dns = Setup(ntuple(α -> LinRange(lims..., ndns + 1), D)...; Re, ArrayType);
-les = Setup(ntuple(α -> LinRange(lims..., nles + 1), D)...; Re, ArrayType);
+
+filters = map([
+    (FaceAverage(), 64),
+    (FaceAverage(), 128),
+    (FaceAverage(), 256),
+    (VolumeAverage(), 64),
+    (VolumeAverage(), 128),
+    (VolumeAverage(), 256),
+]) do (Φ, nles)
+    compression = ndns ÷ nles
+    setup = Setup(ntuple(α -> LinRange(lims..., nles + 1), D)...; Re, ArrayType)
+    psolver = SpectralPressureSolver(setup)
+    (; setup, Φ, compression, psolver)
+end;
 
 # Since the grid is uniform and identical for x and y, we may use a specialized
 # spectral pressure solver
 psolver_dns = SpectralPressureSolver(dns);
-psolver_les = SpectralPressureSolver(les);
 
 # Create random initial conditions
-u₀ = random_field(dns, T(0); psolver);
+u₀ = random_field(dns, T(0); psolver = psolver_dns);
 
 # Solve unsteady problem
 state, outputs = solve_unsteady(
     dns,
     u₀,
-    (T(0), T(0.1));
-    Δt = T(1e-3),
-    psolver_dns,
+    (T(0), T(0.01));
+    Δt = T(1e-4),
+    psolver = psolver_dns,
     processors = (
         rtp = realtimeplotter(; setup = dns, nupdate = 1),
+        obs = observe_u(dns, psolver_dns, filters),
         # ehist = realtimeplotter(;
         #     setup,
         #     plot = energy_history_plot,
@@ -67,9 +163,40 @@ state, outputs = solve_unsteady(
 );
 (; u, t) = state;
 
-ubar = IncompressibleNavierStokes.face_average(u, les, comp);
-ubar = IncompressibleNavierStokes.volume_average(u, les, comp);
+begin
+    println("Φ\t\tM\tDu\tPv\tPc\tc")
+    for o in outputs.obs
+        nt = length(o.t)
+        Dv = sum(o.Dv) / nt
+        Pc = sum(o.Pc) / nt
+        Pv = sum(o.Pv) / nt
+        c = sum(o.c) / nt
+        @printf(
+            # "%s\t%d^%d\t%.2g\t%.2g\t%.2g\t%.2g\n",
+            "%s &\t\$%d^%d\$ &\t\$%.2g\$ &\t\$%.2g\$ &\t\$%.2g\$ &\t\$%.2g\$\n",
+            typeof(o.Φ),
+            o.Mα,
+            D,
+            Dv,
+            Pv,
+            Pc,
+            c
+        )
+    end
+end;
+
+o = outputs.obs[1]
+o.Dv
+o.Pc
+o.Pv
+o.c
+
+# apply_bc_u!(u, t, dns)
+ubar = FaceAverage()(u, les, comp);
+ubar = VolumeAverage()(u, les, comp);
+# apply_bc_u!(ubar, t, les)
 fieldplot((; u = ubar, t); setup = les)
+fieldplot((; u, t); setup = dns)
 
 IncompressibleNavierStokes.apply_bc_u!(ubar, t, les)
 div = IncompressibleNavierStokes.divergence(ubar, les)[les.grid.Ip]
