df42e7eb4342cfd7fe4171501dc345854108805b
Syver Døving Agdestein <syverda@gmail.com>
2024-04-19 14:57:46 +0200

    Old updates

--- src/closures/fno.jl
+++ src/closures/fno.jl
@@ -27,6 +27,8 @@ function fno(; setup, kmax, c, σ, ψ, rng = Random.default_rng(), kwargs...)
         # Put inputs in pressure points
         collocate,
 
+        # Conv(ntuple(Returns(1), D), D => c[1]; use_bias = false, init_weight),
+
         # Some Fourier layers
         (
             FourierLayer(dimension, kmax[i], c[i] => c[i+1]; σ = σ[i], init_weight) for
@@ -34,9 +36,9 @@ function fno(; setup, kmax, c, σ, ψ, rng = Random.default_rng(), kwargs...)
         )...,
 
         # Compress with a final dense layer
-        Conv(ntuple(Returns(1), D), c[end] => D; use_bias = false),
-        # Conv(ntuple(Returns(1), D), c[end] => 2 * c[end], ψ; use_bias = false),
-        # Conv(ntuple(Returns(1), D), 2 * c[end] => D; use_bias = false),
+        # Conv(ntuple(Returns(1), D), c[end] => D; use_bias = false, init_weight),
+        Conv(ntuple(Returns(1), D), c[end] => 2 * c[end], ψ; init_weight),
+        Conv(ntuple(Returns(1), D), 2 * c[end] => D; use_bias = false, init_weight),
 
         # Differentiate output to velocity points
         decollocate,
@@ -82,11 +84,14 @@ Lux.initialparameters(
     (; dimension, kmax, cin, cout, init_weight)::FourierLayer,
 ) = (;
     spatial_weight = init_weight(rng, cout, cin),
-    spectral_weights = init_weight(rng, fill(kmax + 1, dimension())..., cout, cin, 2),
+    # spectral_weights = init_weight(rng, fill(kmax + 1, dimension())..., cout, cin, 2),
+    spectral_weights = init_weight(rng, fill(2 * (kmax + 1), dimension())..., cout, cin, 2),
 )
 Lux.initialstates(::AbstractRNG, ::FourierLayer) = (;)
+# Lux.parameterlength((; dimension, kmax, cin, cout)::FourierLayer) =
+#     cout * cin + (kmax + 1)^dimension() * 2 * cout * cin
 Lux.parameterlength((; dimension, kmax, cin, cout)::FourierLayer) =
-    cout * cin + (kmax + 1)^dimension() * 2 * cout * cin
+    cout * cin + (2 * (kmax + 1))^dimension() * 2 * cout * cin
 Lux.statelength(::FourierLayer) = 0
 
 ## Pretty printing
@@ -110,6 +115,7 @@ function ((; dimension, kmax, cout, cin, σ)::FourierLayer)(x, params, state)
     D = dimension()
 
     nx..., _cin, nsample = size(x)
+    K = nx[1]
     @assert _cin == cin "Number of input channels must be compatible with weights"
     @assert all(==(first(nx)), nx) "Fourier layer requires same number of grid points in each dimension"
     @assert kmax ≤ first(nx) "Fourier layer input must be discretized on at least `kmax` points"
@@ -136,16 +142,56 @@ function ((; dimension, kmax, cout, cin, σ)::FourierLayer)(x, params, state)
     # - multiply with weights mode-wise
     # - pad with zeros to restore original shape
     # - go back to real valued spatial representation
-    ikeep = ntuple(Returns(1:kmax+1), D)
+    ikeep = ntuple(Returns([1:kmax+1; K-kmax:K]), D)
+    # ikeep = ntuple(Returns(1:kmax+1), D)
     dims = ntuple(identity, D)
     xhat = fft(x, dims)
     xhat = xhat[ikeep..., :, :]
     if D == 2
         @tullio z[k₁, k₂, b, s] := R[k₁, k₂, b, a] * xhat[k₁, k₂, a, s]
+        z = cat(
+            z[1:kmax+1, :, :, :],
+            zero(similar(z, K - 2 * (kmax + 1), 2 * (kmax + 1), cout, nsample)),
+            z[end-kmax:end, :, :, :];
+            dims = 1,
+        )
+        z = cat(
+            z[:, 1:kmax+1, :, :],
+            zero(similar(z, K, K - 2 * (kmax + 1), cout, nsample)),
+            z[:, end-kmax:end, :, :];
+            dims = 2,
+        )
     elseif D == 3
         @tullio z[k₁, k₂, k₃, b, s] := R[k₁, k₂, k₃, b, a] * xhat[k₁, k₂, k₃, a, s]
+        z = cat(
+            z[1:kmax+1, :, :, :, :],
+            zero(
+                similar(
+                    z,
+                    K - 2 * (kmax + 1),
+                    2 * (kmax + 1),
+                    2 * (kmax + 1),
+                    cout,
+                    nsample,
+                ),
+            ),
+            z[end-kmax:end, :, :, :, :];
+            dims = 1,
+        )
+        z = cat(
+            z[:, 1:kmax+1, :, :, :],
+            zero(similar(z, K, K - 2 * (kmax + 1), 2 * (kmax + 1), cout, nsample)),
+            z[:, end-kmax:end, :, :, :];
+            dims = 2,
+        )
+        z = cat(
+            z[:, :, 1:kmax+1, :, :],
+            zero(similar(z, K, K, K - 2 * (kmax + 1), cout, nsample)),
+            z[:, :, end-kmax:end, :, :];
+            dims = 3,
+        )
     end
-    z = pad_zeros(z, ntuple(i -> isodd(i) ? 0 : first(nx) - kmax - 1, 2N); dims)
+    # z = pad_zeros(z, ntuple(i -> isodd(i) ? 0 : first(nx) - kmax - 1, 2 * D); dims)
     z = real.(ifft(z, dims))
 
     # Outer layer: Activation over combined spatial and spectral parts
