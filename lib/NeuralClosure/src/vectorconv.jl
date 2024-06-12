502b9017914aa5868195b5e0deb788605727ce67
Syver Døving Agdestein <syverda@gmail.com>
2024-06-12 16:44:05 +0200

    Up

--- lib/NeuralClosure/src/vectorconv.jl
+++ lib/NeuralClosure/src/vectorconv.jl
@@ -1,11 +1,10 @@
 struct VectorConv2D{C,A}
     conv::C
     activation::A
-end
-
-function VectorConv2D(args...; activation, kwargs...)
-    conv = Conv(args...; kwargs...)
-    VectorConv2D{typeof(conv),typeof(activation)}(conv, activation)
+    function VectorConv2D(args...; activation, kwargs...)
+        conv = Conv(args...; kwargs...)
+        new{typeof(conv),typeof(activation)}(conv, activation)
+    end
 end
 
 Lux.initialparameters(rng::AbstractRNG, vc::VectorConv2D) = Lux.initialparameters(rng, vc.conv)
@@ -13,14 +12,25 @@ Lux.initialstates(::AbstractRNG, vc::VectorConv2D) = Lux.initialstates(rng, vc.c
 Lux.parameterlength(vc::VectorConv2D) = Lux.parameterlength(vc.conv)
 Lux.statelength(vc::VectorConv2D) = Lux.statelength(vc.conv)
 
+function rotate(w)
+    s = size(w, 1)
+    i = 1:s
+    w[i, j, ch]
+end
+
 function (vc::VectorConv2D)(xy, params, state)
     (; conv, activation) = vc
+    (; weight) = params
     x, y = xy
-    cux, _ = conv(x, params, state)
-    cuy, _ = conv(y, params, state)
-    cvx, _ = conv(x, rotate(rotate(params)), state)
-    cvy, _ = conv(y, rotate(params), state)
+    wux = weight
+    wuy = rotate(weight)
+    wvx = rotate(rotate(weight))
+    wvy = rotate(weight)
+    cux, _ = conv(x, (; params..., weight = wux), state)
+    cuy, _ = conv(y, (; params..., weight = wuy), state)
+    cvx, _ = conv(x, (; params..., weight = wvx), state)
+    cvy, _ = conv(y, (; params..., weight = wvy), state)
     u = activation.(cux .+ cuy)
-    v = activation.(cvy .+ cvy)
+    v = activation.(cvx .+ cvy)
     (u, v), state
 end
--- lib/TensorClosure/vectorconv.jl
+++ lib/TensorClosure/vectorconv.jl
@@ -1,5 +1,9 @@
 using IncompressibleNavierStokes
 using NeuralClosure
 using Lux
+using Random
+rng = Xoshiro()
 
-
+conv = Conv((5,), 2 => 8)
+params, state = Lux.setup(rng, conv)
+params
