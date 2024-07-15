c640c2fa35bdba93e10cbb42e44d30bf5850fec2
Syver Døving Agdestein <syverda@gmail.com>
2024-07-15 16:09:36 +0200

    Update example

--- lib/SciMLCompat/src/SciMLCompat.jl
+++ lib/SciMLCompat/src/SciMLCompat.jl
@@ -1,5 +1,24 @@
 module SciMLCompat
 
-using IncompressibleNavierStokes
+import IncompressibleNavierStokes as INS
+
+"""
+    create_right_hand_side(setup, psolver)
+
+Create right hand side function f(u, p, t) compatible with
+the OrdinaryDiffEq ODE solvers. Note that `u` has to be an array.
+To convert the tuple `u = (ux, uy)` to an array, use `stack(u)`.
+"""
+create_right_hand_side(setup, psolver) = function right_hand_side(u, p, t)
+    u = eachslice(u; dims = 3)
+    u = (u...,)
+    u = INS.apply_bc_u(u, t, setup)
+    F = INS.momentum(u, nothing, t, setup)
+    F = INS.apply_bc_u(F, t, setup; dudt = true)
+    PF = INS.project(F, setup; psolver)
+    stack(PF)
+end
+
+export create_right_hand_side
 
 end
