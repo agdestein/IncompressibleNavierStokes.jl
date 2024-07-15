383cf200c6731f12bccc6e16d27d1e79cba18ae5
Syver Døving Agdestein <syverda@gmail.com>
2024-07-15 17:19:59 +0200

    feat(SciML): Add in-place version

--- lib/SciMLCompat/src/SciMLCompat.jl
+++ lib/SciMLCompat/src/SciMLCompat.jl
@@ -19,6 +19,32 @@ create_right_hand_side(setup, psolver) = function right_hand_side(u, p, t)
     stack(PF)
 end
 
-export create_right_hand_side
+"""
+    create_right_hand_side_inplace(setup, psolver)
+
+In place version of [`create_right_hand_side`](@ref).
+"""
+function create_right_hand_side_inplace(setup, psolver)
+    (; x, N, dimension) = setup.grid
+    D = dimension()
+    v = ntuple(α -> similar(x[1], N), D)
+    F = ntuple(α -> similar(x[1], N), D)
+    div = similar(x[1], N)
+    p = similar(x[1], N)
+    function right_hand_side!(dudt, u, params, t)
+        u = eachslice(u; dims = ndims(u))
+        copyto!.(v, (u...,))
+        INS.apply_bc_u!(v, t, setup)
+        INS.momentum!(F, v, nothing, t, setup)
+        INS.apply_bc_u!(F, t, setup; dudt = true)
+        INS.project!(F, setup; psolver, div, p)
+        for α in 1:D
+            dudt[ntuple(Returns(:), D)..., α] .= F[α]
+        end
+        dudt
+    end
+end
+
+export create_right_hand_side, create_right_hand_side_inplace
 
 end
