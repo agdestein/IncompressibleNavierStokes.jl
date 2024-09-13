33ce53a97d821b41fb74afdc98d1cd3219af70b8
Syver Døving Agdestein <syverda@gmail.com>
2024-09-13 20:18:10 +0200

    refactor: Update example

--- examples/DecayingTurbulence3D.jl
+++ examples/DecayingTurbulence3D.jl
@@ -10,70 +10,42 @@
 using GLMakie #!md
 using IncompressibleNavierStokes
 
-# Output directory
-outdir = joinpath(@__DIR__, "output", "DecayingTurbulence3D")
-ispath(outdir) || mkpath(outdir)
+# ## Problem setup
 
-# Floating point precision
-T = Float64
-
-# Array type
 ArrayType = Array
 ## using CUDA; ArrayType = CuArray
-## using AMDGPU; ArrayType = ROCArray
-## using oneAPI; ArrayType = oneArray
-## using Metal; ArrayType = MtlArray
-
-# Reynolds number
-Re = T(6_000)
-
-# A 3D grid is a Cartesian product of three vectors
-n = 32
-lims = T(0), T(1)
-x = LinRange(lims..., n + 1), LinRange(lims..., n + 1), LinRange(lims..., n + 1)
-
-# Build setup and assemble operators
-setup = Setup(; x, Re, ArrayType);
-
-# Since the grid is uniform and identical for x, y, and z, we may use a
-# specialized spectral pressure solver
-psolver = psolver_spectral(setup);
-
-# Initial conditions
+T = Float32
+n = 128
+ax = range(T(0), T(1), n + 1)
+setup = Setup(; x = (ax, ax, ax), Re = T(4e3), ArrayType);
+psolver = psolver_spectral_lowmemory(setup);
 ustart = random_field(setup; psolver);
 
-# Solve unsteady problem
-(; u, t), outputs = solve_unsteady(;
+# ## Solve problem
+
+state, outputs = solve_unsteady(;
     setup,
     ustart,
-    tlims = (T(0), T(1)),
-    Δt = T(1e-3),
+    tlims = (T(0), T(2)),
     psolver,
     processors = (
         ## rtp = realtimeplotter(; setup, plot = fieldplot, nupdate = 10),
-        ehist = realtimeplotter(;
-            setup,
-            plot = energy_history_plot,
-            nupdate = 10,
-            displayfig = false,
-        ),
+        ## ehist = realtimeplotter(; setup, plot = energy_history_plot, nupdate = 10),
         espec = realtimeplotter(; setup, plot = energy_spectrum_plot, nupdate = 10),
         ## anim = animator(; setup, path = "$outdir/solution.mkv", nupdate = 20),
         ## vtk = vtk_writer(; setup, nupdate = 10, dir = outdir, filename = "solution"),
-        ## field = fieldsaver(; setup, nupdate = 10),
-        log = timelogger(; nupdate = 100),
+        log = timelogger(; nupdate = 10),
     ),
 );
 
 # ## Post-process
-#
-# We may visualize or export the computed fields `(u, p)`
+
+# Field plot
+## outputs.rtp
+## fieldplot(state; setup, levels = 0:5)
 
 # Energy history
-outputs.ehist
+## outputs.ehist
 
 # Energy spectrum
 outputs.espec
-
-# Export to VTK
-save_vtk(state; setup, filename = joinpath(outdir, "solution"), psolver)
