9603b0c37f00d9da0d941e14e7b087f5425671f8
Syver Døving Agdestein <syverda@gmail.com>
2024-09-13 20:19:19 +0200

    feat(PaperDC): add function for splitting seed

--- lib/PaperDC/src/PaperDC.jl
+++ lib/PaperDC/src/PaperDC.jl
@@ -5,17 +5,22 @@ module PaperDC
 
 using DocStringExtensions
 using EnumX
+using LinearAlgebra
 using IncompressibleNavierStokes
 using IncompressibleNavierStokes: momentum!, divergence!, project!, apply_bc_u!
 using Observables
-using LinearAlgebra
+using Random
 
 # Inherit docstring templates
 @template (MODULES, FUNCTIONS, METHODS, TYPES) = IncompressibleNavierStokes
 
+"Split random number generator seed into `n` new seeds."
+splitseed(seed, n) = rand(Xoshiro(seed), UInt32, n)
+
 include("observe.jl")
 include("rk.jl")
 
+export splitseed
 export observe_u, observe_v
 export ProjectOrder, RKProject
 
